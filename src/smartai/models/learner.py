9edcf0d30bc8948e09363b00f140333ab15e8ca8
yajiez <yajie.zhu@georgeinstitute.ox.ac.uk>
2018-11-16 11:21:58 +0000

    code: Add a lot of placeholders

---
+++ src/smartai/models/learner.py
@@ -0,0 +1,25 @@
+"""Learner
+"""
+
+class BaseLearner(object):
+    pass
+
+
+class SmartLearner(BaseLearner):
+    pass
+
+
+class ImageLearner(BaseLearner):
+    pass
+
+
+class VideoLearn(BaseLearner):
+    pass
+
+
+class NLPLearner(BaseLearner):
+    pass
+
+
+class TabularLearner(BaseLearner):
+    pass
