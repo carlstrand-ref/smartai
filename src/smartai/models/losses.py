96b53e6dfce9836c1ac67d4df5d5681c2a10aee5
yajiez <yajie.zhu@georgeinstitute.ox.ac.uk>
2018-11-16 11:26:45 +0000

    code: Add PyTorch built-in losses

---
+++ src/smartai/models/losses.py
@@ -0,0 +1,16 @@
+from collections import namedtuple as _namedtuple
+import torch as _torch
+
+__all__ = [
+    'TORCH_LOSSES'
+]
+
+_torch_losses = {
+    c: _torch.nn.modules.loss.__dict__[c] for c in dir(_torch.nn.modules.loss)
+    if c.endswith('Loss') and (not c.startswith('_'))
+}
+locals().update(_torch_losses)
+print("PyTorch has {} builtin loss Classes.".format(len(_torch_losses)))
+
+_Torch_Losses = _namedtuple('Torch_Losses', _torch_losses.keys())
+TORCH_LOSSES = _Torch_Losses(**_torch_losses)
