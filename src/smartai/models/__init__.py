a66993d4aef14c703f2c7950463f3fdbb23d78b7
yajiez <yajie.zhu@georgeinstitute.ox.ac.uk>
2018-11-19 10:40:54 +0000

    code: Add a subpackage learner
    Learner is an intelligent agent contains datasets, models, and
    many other resources and abilities.

--- src/smartai/models/__init__.py
+++ src/smartai/models/__init__.py
@@ -2,7 +2,6 @@ from . import nn
 from . import losses
 from . import metrics
 from . import optimizers
-from . import learner
 
 from .losses import TORCH_LOSSES
 from .optimizers import TORCH_OPTIMIZER
